5f5561307a4183269d3689aea6f56a5f24d5e71a
nik frank <nikbfrank@gmail.com>
2019-02-13 18:25:35 +0200

    pieces demo

--- README.md
+++ README.md
@@ -93,5 +93,6 @@ export default App;
 ```
 
 
+https://css-tricks.com/snippets/css/css-triangle/
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/Board.js
+++ src/Board.js
@@ -8,7 +8,13 @@ class App extends Component {
         <div className="topRow">
           <div className="p2-home"/>
           
-          <div className="pip"/>
+          <div className="pip">
+            <svg viewBox='0 0 100 1000' style={{ position: 'absolute', left: 0, zIndex: 100}}>
+              {Array(5).fill(0).map((o, i)=>(
+                <circle key={i} cx={50} cy={26 + 53*i} r={25} fill='red' stroke='black' strokeWidth={3}/>
+              ))}
+            </svg>
+          </div>
           <div className="pip"/>
           <div className="pip"/>
           <div className="pip"/>
