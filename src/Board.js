8d8e749579e07ae60440ba029792dae967fe650c
nik frank <nikbfrank@gmail.com>
2019-02-13 18:43:32 +0200

    refactor board and render opening position

--- src/Board.js
+++ src/Board.js
@@ -1,53 +1,73 @@
 import React, { Component } from 'react';
 import './Board.css';
 
-class App extends Component {
+class Board extends Component {
   render() {
+    const { pieces: [p1, p2] } = this.props;
+    
     return (
       <div className="Board">
         <div className="topRow">
           <div className="p2-home"/>
-          
-          <div className="pip">
-            <svg viewBox='0 0 100 1000' style={{ position: 'absolute', left: 0, zIndex: 100}}>
-              {Array(5).fill(0).map((o, i)=>(
-                <circle key={i} cx={50} cy={26 + 53*i} r={25} fill='red' stroke='black' strokeWidth={3}/>
-              ))}
-            </svg>
-          </div>
-          <div className="pip"/>
-          <div className="pip"/>
-          <div className="pip"/>
-          <div className="pip"/>
-          <div className="pip"/>
+
+          {Array(6).fill(0).map((o,j)=>(
+            <div className="pip">
+              <svg viewBox='0 0 100 1000' style={{ position: 'absolute', left: 0, zIndex: 100}}>
+                {p1.filter(p=> p===j+1).map((o, i)=>(
+                  <circle key={i} cx={50} cy={26 + 53*i} r={25} fill='red' stroke='black' strokeWidth={3}/>
+                ))}
+                {p2.filter(p=> p===j+1).map((o, i)=>(
+                  <circle key={i} cx={50} cy={26 + 53*i} r={25} fill='blue' stroke='black' strokeWidth={3}/>
+                ))}
+              </svg>
+            </div>
+          ))}
           
           <div className="jail"/>
 
-          <div className="pip"/>
-          <div className="pip"/>
-          <div className="pip"/>
-          <div className="pip"/>
-          <div className="pip"/>
-          <div className="pip"/>
+          {Array(6).fill(0).map((o,j)=>(
+            <div className="pip">
+              <svg viewBox='0 0 100 1000' style={{ position: 'absolute', left: 0, zIndex: 100}}>
+                {p1.filter(p=> p===j+7).map((o, i)=>(
+                  <circle key={i} cx={50} cy={26 + 53*i} r={25} fill='red' stroke='black' strokeWidth={3}/>
+                ))}
+                {p2.filter(p=> p===j+7).map((o, i)=>(
+                  <circle key={i} cx={50} cy={26 + 53*i} r={25} fill='blue' stroke='black' strokeWidth={3}/>
+                ))}
+              </svg>
+            </div>
+          ))}
         </div>
 
         <div className="bottomRow">
-          <div className="pip"/>
-          <div className="pip"/>
-          <div className="pip"/>
-          <div className="pip"/>
-          <div className="pip"/>
-          <div className="pip"/>
+          {Array(6).fill(0).map((o,j)=>(
+            <div className="pip">
+              <svg viewBox='0 0 100 500' style={{ position: 'absolute', left: 0, zIndex: 100}}>
+                {p1.filter(p=> p===j+13).map((o, i)=>(
+                  <circle key={i} cx={50} cy={26 + 53*i} r={25} fill='red' stroke='black' strokeWidth={3}/>
+                ))}
+                {p2.filter(p=> p===j+13).map((o, i)=>(
+                  <circle key={i} cx={50} cy={26 + 53*i} r={25} fill='blue' stroke='black' strokeWidth={3}/>
+                ))}
+              </svg>
+            </div>
+          ))}
           
           <div className="jail"/>
 
-          <div className="pip"/>
-          <div className="pip"/>
-          <div className="pip"/>
-          <div className="pip"/>
-          <div className="pip"/>
-          <div className="pip"/>
-
+          {Array(6).fill(0).map((o,j)=>(
+            <div className="pip">
+              <svg viewBox='0 0 100 500' style={{ position: 'absolute', height: '100%', left: 0, zIndex: 100}}>
+                {p1.filter(p=> p===j+19).map((o, i)=>(
+                  <circle key={i} cx={50} cy={26 + 53*i} r={25} fill='red' stroke='black' strokeWidth={3}/>
+                ))}
+                {p2.filter(p=> p===j+19).map((o, i)=>(
+                  <circle key={i} cx={50} cy={26 + 53*i} r={25} fill='blue' stroke='black' strokeWidth={3}/>
+                ))}
+              </svg>
+            </div>
+          ))}
+          
           <div className="p1-home"/>
         </div>
 
@@ -57,4 +77,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Board;
